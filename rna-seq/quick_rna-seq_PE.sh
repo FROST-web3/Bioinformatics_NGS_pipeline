2a629b4442cd111cb2c78e373334abcf76f0b007
FROST <164864298+FROST-web3@users.noreply.github.com>
2025-02-26 21:54:59 +0800

    Update quick_rna-seq_PE.sh

--- rna-seq/quick_rna-seq_PE.sh
+++ rna-seq/quick_rna-seq_PE.sh
@@ -8,14 +8,14 @@ hisat2() {
     cd "$input_dir"
     ls -1 *.gz| xargs -n2 | while read r1 r2; do 
     sample_name=${r1%.fastq*}
-    echo "hisat2 -x /ifs1/User/FROST/my_work/rna-seq/ref/S_lycopersicum_chromosomes.4.00.fa -1 ${r1} -2 ${r2} -S ${sample_name}.sam -p 28;
+    echo "hisat2 -x /ifs1/User/my_work/rna-seq/ref/S_lycopersicum_chromosomes.4.00.fa -1 ${r1} -2 ${r2} -S ${sample_name}.sam -p 28;
     samtools sort -@ 28 "${sample_name}.sam" -o "${sample_name}.sorted.bam";
     samtools index -@ 28 "${sample_name}.sorted.bam";
     rm -f "${sample_name}.sam";">> hisat.sh;
 
     done;
     sh hisat.sh;
-    featureCounts -g gene_id -a /ifs1/User/mahaifeng/my_work/rna-seq/ref/ITAG4.0_gene_models.gtf --primary -T 28 -p -o CountMatrix.txt *.bam
+    featureCounts -g gene_id -a /ifs1/User/my_work/rna-seq/ref/ITAG4.0_gene_models.gtf --primary -T 28 -p -o CountMatrix.txt *.bam
     
 
     cd "$initial_dir"
@@ -35,4 +35,4 @@ main() {
 
 main "$@"
 
-#featureCounts -g gene_id -a /ifs1/User/mahaifeng/my_work/rna-seq/ref/ITAG4.0_gene_models.gtf --primary -T 28 -p -o CountMatrix.txt *.bam
+#featureCounts -g gene_id -a /ifs1/User/my_work/rna-seq/ref/ITAG4.0_gene_models.gtf --primary -T 28 -p -o CountMatrix.txt *.bam
